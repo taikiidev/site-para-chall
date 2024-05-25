8c7fde0f1cafaf2127389060700021d26a73c695
Vini <taiki.code@gmail.com>
2024-05-24 21:39:27 -0300

    hehe quase fiz merda

--- src/pages/Contato/index.jsx
+++ src/pages/Contato/index.jsx
@@ -1,9 +1,31 @@
 function Contato() {
     return (  
-        <div className="mt-16">
-            <h1>Contato</h1>
-        </div>
-    );
-}
+      <>
+        <section className="bg-white mt-30 h-screen">
+          <div className=" mt-40 mx-auto max-w-screen-md">
+            <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-black mt-30">Fale conosco!</h2>
+            <p className="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-700 sm:text-xl">Tem uma sugestão ou reclamação? Deixe sua mensagem!</p>
+            <form action="#" className="space-y-8 p-10 rounded border-black border-2">
+              <div>
+                <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-700">Seu Email</label>
+                <input type="email" id="email" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="cavalobacana@gmail.com" required />
+              </div>
+              <div>
+                <label htmlFor="subject" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-700">Assunto</label>
+                <input type="text" id="subject" className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="Como podemos ajudar?" required />
+              </div>
+              <div className="sm:col-span-2">
+                <label htmlFor="message" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-700">Sua mensagem</label>
+                <textarea id="message" rows="6" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Deixe seu comentário"></textarea>
+              </div>
+              <button type="submit" className="py-3 px-5 text-sm font-medium text-center text-white rounded-lg bg-gray-700 sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Enviar mensagem</button>
 
-export default Contato;
+            </form>
+          </div>
+        </section> 
+      </>
+    );
+  }
+  
+  export default Contato;
+  
--- src/pages/Funcionamento/index.jsx
+++ src/pages/Funcionamento/index.jsx
@@ -1,8 +1,49 @@
 function Funcionamento() {
     return (  
-        <div className="mt-16">
-            <h1>funcionamento</h1>
-        </div>
+        <>
+
+            <div className="w-3/4 m-auto mt-40">
+
+                            <div className="flex justify-between items-center">
+                    <p className="w-1/3 ml-48">Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis deserunt eos architecto omnis! Minus quis obcaecati molestias, cupiditate facere quisquam iure reprehenderit, aperiam, voluptatibus iste blanditiis eveniet velit animi a!</p>
+                    <img className="mr-48" width='500px' src="src\assets\Screenshot_1.png" alt="imagem mostrando a tela inicial" />
+                </div>
+
+
+
+
+
+
+
+                <div className="flex justify-between mt-24">
+
+
+                    
+                    <img width='500px'  className="ml-48 " src="src\assets\Screenshot_2.png" alt="imagem mostrando a criação de personagem" />
+                    <p className="w-1/3 mr-48">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur, quaerat dolore minus vel ev
+                        eniet error repudiandae tempore quos autem illum qui unde ipsa saepe esse maxime ducimus itaque! Impedit, voluptates?</p>
+
+                </div>
+
+
+
+
+
+
+
+
+                <div className="flex justify-between mt-24"> 
+
+
+                    <p className="w-1/3 ml-48"> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur, quaerat dolore minus vel ev
+                        eniet error repudiandae tempore quos autem illum qui unde ipsa saepe esse maxime ducimus itaque! Impedit, voluptates?</p>
+                    <img width='500px' className="mr-48" src="src\assets\Screenshot_3.png" alt="imagem mostrando aba de vídeos" />
+
+
+                </div>
+            </div>
+
+        </>
     );
 }
 
