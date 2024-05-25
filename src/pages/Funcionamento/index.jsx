f9f5c6cec0e10989a457e5996141de65ba517ac5
dagalera <dagalera.dev@gmail.com>
2024-05-24 21:50:14 -0300

    att final

--- src/pages/Funcionamento/index.jsx
+++ src/pages/Funcionamento/index.jsx
@@ -5,7 +5,7 @@ function Funcionamento() {
             <div className="w-3/4 m-auto mt-40">
 
                             <div className="flex justify-between items-center">
-                    <p className="w-1/3 ml-48">Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis deserunt eos architecto omnis! Minus quis obcaecati molestias, cupiditate facere quisquam iure reprehenderit, aperiam, voluptatibus iste blanditiis eveniet velit animi a!</p>
+                    <p className="w-1/3 ml-48">Bem-vindo ao nosso portal! Aqui na página inicial, você encontrará um mundo de possibilidades para tornar sua jornada hospitalar mais interativa e informativa. Explore nossos recursos, incluindo a criação de personagens, acesso aos seus vídeos personalizados e uma seleção de jogos divertidos para entreter enquanto você aprende. Prepare-se para uma experiência única e educativa!</p>
                     <img className="mr-48" width='500px' src="src\assets\Screenshot_1.png" alt="imagem mostrando a tela inicial" />
                 </div>
 
@@ -20,8 +20,7 @@ function Funcionamento() {
 
                     
                     <img width='500px'  className="ml-48 " src="src\assets\Screenshot_2.png" alt="imagem mostrando a criação de personagem" />
-                    <p className="w-1/3 mr-48">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur, quaerat dolore minus vel ev
-                        eniet error repudiandae tempore quos autem illum qui unde ipsa saepe esse maxime ducimus itaque! Impedit, voluptates?</p>
+                    <p className="w-1/3 mr-48">Dê vida ao seu avatar! Na nossa seção de criação de personagens, você tem a oportunidade de personalizar seu próprio representante virtual. Escolha entre uma variedade de opções de cabelo, roupas e acessórios para criar um personagem que seja verdadeiramente seu. Assim, você estará pronto para embarcar em uma jornada de aprendizado enquanto se diverte!</p>
 
                 </div>
 
@@ -35,8 +34,7 @@ function Funcionamento() {
                 <div className="flex justify-between mt-24"> 
 
 
-                    <p className="w-1/3 ml-48"> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur, quaerat dolore minus vel ev
-                        eniet error repudiandae tempore quos autem illum qui unde ipsa saepe esse maxime ducimus itaque! Impedit, voluptates?</p>
+                    <p className="w-1/3 ml-48">Explore um universo de conhecimento sob medida para você! Em nossa página de vídeos, cada paciente recebe uma experiência personalizada, adaptada aos exames e procedimentos prescritos por seu médico. Nossos vídeos explicativos fornecem informações claras e relevantes, ajudando a compreender melhor os processos médicos. Prepare-se para se sentir mais preparado e confiante em sua jornada de tratamento.</p>
                     <img width='500px' className="mr-48" src="src\assets\Screenshot_3.png" alt="imagem mostrando aba de vídeos" />
 
 
